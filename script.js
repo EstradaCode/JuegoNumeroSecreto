2f08af8f888f7d44893cae7004cb6806dbb68c2b
Francisco <franciscoestrada565@gmail.com>
2024-01-29 15:09:35 -0300

    Update 1.3 -> Js- html se utilizaron eventlisteners en vez de onclicks, y textContent en vez de innerHTML.

--- script.js
+++ script.js
@@ -15,28 +15,28 @@ function valoresDefault(){
         numeroSecreto = Math.floor(Math.random()*10 + 1);
     }
         intentos = 1;
-        document.querySelector('#show').innerHTML = 'Escribe un numero del 1 al 10!';
-        document.getElementById('num').innerHTML = maxIntentos;
+        document.querySelector('#show').textContent = 'Escribe un numero del 1 al 10!';
+        document.getElementById('num').textContent = maxIntentos;
         document.querySelector('input').value = '';
-        document.querySelector('h3').innerHTML ='';
+        document.querySelector('h3').textContent ='';
         document.getElementById('reiniciar').setAttribute('disabled',true);
         document.getElementById('intentar').removeAttribute('disabled');
     }
 function actualizarIntentos(){
     if(maxIntentos >0){
-        document.querySelector('#num').innerHTML = --maxIntentos;
+        document.querySelector('#num').textContent = --maxIntentos;
         intentos++;
     }
 }
 function recompensaIntentos(){
     maxIntentos++;
-    document.querySelector('h3').innerHTML = 'Ganaste un intento más! para tu proxima ronda';
+    document.querySelector('h3').textContent = 'Ganaste un intento más! para tu proxima ronda';
 }
 function verificarNumero(){
     numero = parseInt(document.querySelector('input').value);
     console.log(numero); 
     if(numero === numeroSecreto){
-        document.getElementById('show').innerHTML = `GANASTE! ADIVINASTE EL NUMERO SECRETO despues de ${intentos} ${intentos==1? 'vez' : 'veces'}`;
+        document.getElementById('show').textContent = `GANASTE! ADIVINASTE EL NUMERO SECRETO despues de ${intentos} ${intentos==1? 'vez' : 'veces'}`;
         document.getElementById('reiniciar').removeAttribute('disabled');
         document.getElementById('intentar').setAttribute('disabled',true);
         if(numerosEncontrados.length != 10){
@@ -44,20 +44,26 @@ function verificarNumero(){
             numerosEncontrados.push(numero);
         }else{
             document.getElementById('intentar').setAttribute('disabled',true);
-            document.querySelector('h3').innerHTML = 'ENCONTRASTE LOS 10 NUMEROS! FELICIDADES POR COMPLETAR EL JUEGO AL 100%. Vuelve a jugarlo cuando quieras!';
+            document.querySelector('h3').textContent = 'ENCONTRASTE LOS 10 NUMEROS! FELICIDADES POR COMPLETAR EL JUEGO AL 100%. Vuelve a jugarlo cuando quieras!';
         }
     }else{
         if(numero > numeroSecreto){
-            document.getElementById('show').innerHTML = 'Cerca! El Numero Secreto es Menor!';
+            document.getElementById('show').textContent = 'Cerca! El Numero Secreto es Menor!';
             actualizarIntentos();
         } else{
-            document.getElementById('show').innerHTML = 'Cerca! El Numero Secreto es mayor'
+            document.getElementById('show').textContent = 'Cerca! El Numero Secreto es mayor'
             actualizarIntentos();
         }
         if(maxIntentos == 0){
-            document.getElementById('show').innerHTML = 'Perdiste! Te quedaste sin intentos';   
-            document.getElementById('num').innerHTML = '0';
+            document.getElementById('show').textContent = 'Perdiste! Te quedaste sin intentos';   
+            document.getElementById('num').textContent = '0';
             document.getElementById('intentar').setAttribute('disabled',true);
         }
     }
 }
+const btnIntentar = document.getElementById("intentar").addEventListener("click", () => {
+    verificarNumero();
+})
+const btnReset = document.getElementById("reiniciar").addEventListener("click", () =>{
+    valoresDefault();
+})
